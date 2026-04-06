b0f4a525a775002c5255d4b881a8d0b4923f27ef
shiiru <shiiru@shiiru.dev>
2026-04-06 12:53:43 +0200

    refactor: use custom components for settings

--- src/components/server/tabs/ServerSettings.tsx
+++ src/components/server/tabs/ServerSettings.tsx
@@ -3,6 +3,8 @@ import {MinecraftServer} from "../../../types/types.tsx";
 import { Check, X } from "lucide-react";
 import Button from "../../ui/Button.tsx";
 import { invoke } from "@tauri-apps/api/core";
+import { SettingContainer } from "../../ui/SettingContainer.tsx";
+import { Input } from "../../ui/Input.tsx";
 
 interface ServerSettingsProps { 
     server: MinecraftServer
@@ -19,65 +21,35 @@ export function ServerSettings({ server }: ServerSettingsProps) {
 
     return (
         <div className="flex-1 h-full p-4 bg-neutral-900 flex flex-col space-y-4">
-            <div className={"flex flex-col h-95 max-h-95"}>
-                {/* Java Settings */}
-                <p className={"text-2xl font-medium text-gray-300"}>Java Settings</p>
-
-                <div className={"p-1 py-4 min-h-full h-full space-y-2"}>
-                    {/* Java Path */}
-                    <div className={"h-13 flex flex-row justify-between items-center"}>
-                        <div className={"text-lg text-gray-300"}>
-                            <p className={"font-medium"}>
-                                Java Path
-                            </p>
-                            <p className={"text-sm text-zinc-500"}>
-                                Path to your version-compatible Java executable file. <br/>This will be used to launch the server.
-                            </p>
-                        </div>
-                        <input type={"text"} className={"w-1/2 h-10 py-3 px-2 rounded-lg bg-zinc-800 focus:outline-none placeholder-gray-500 text-gray-300"} placeholder={"C:\\path\\to\\java.exe"} value={settingServer.java_path} onChange={(event) => {
-                            setSettingServer((oldSettingServer) => ({
-                                ...oldSettingServer,
-                                java_path: event.target.value
-                            }))
-                        }}/>
-                    </div>
-
-                    {/* Java Launch Args */}
-                    <div className={"h-13 flex flex-row justify-between items-center"}>
-                        <div className={"text-lg text-gray-300"}>
-                            <p className={"font-medium"}>
-                                Launch Arguments
-                            </p>
-                            <p className={"text-sm text-zinc-500"}>
-                                Java arguments used to launch the server. <br/>
-                            </p>
-                        </div>
-                        <input type={"text"} className={"w-1/2 h-10 py-3 px-2 rounded-lg bg-zinc-800 focus:outline-none placeholder-gray-500 text-gray-300"} placeholder={""} value={settingServer.launch_args} onChange={(event) => {
-                            setSettingServer((oldSettingServer) => ({
-                                ...oldSettingServer,
-                                launch_args: event.target.value
-                            }))
-                        }}/>
-                    </div>
-
-                    {/* Java RAM */}
-                    <div className={"h-13 flex flex-row justify-between items-center"}>
-                        <div className={"text-lg text-gray-300"}>
-                            <p className={"font-medium"}>
-                                Allocated RAM
-                            </p>
-                            <p className={"text-sm text-zinc-500"}>
-                                Maximum RAM that can be used on the server.<br/>
-                            </p>
-                        </div>
-                        <input type={"text"} className={"w-1/2 h-10 py-3 px-2 rounded-lg bg-zinc-800 focus:outline-none placeholder-gray-500 text-gray-300"} placeholder={"4096M"} value={settingServer.allocated_ram} onChange={(event) => {
-                            setSettingServer((oldSettingServer) => ({
-                                ...oldSettingServer,
-                                allocated_ram: event.target.value
-                            }))
-                        }}/>
-                    </div>
-                </div>
+            <p className={"text-2xl font-medium text-gray-300"}>Settings</p>
+            <div className={"flex flex-col p-1 space-y-2 h-95 max-h-95 overflow-y-scroll scrollbar-hide"}>
+                <p className={"text-xl font-medium text-orange-400"}>Java Settings</p>
+                <SettingContainer name={"Java Path"} description={<>
+                    Path to your version-compatible Java executable file. <br/>This will be used to launch the server.
+                </>}>
+                    <Input type={"text"} placeholder={"C\\path\\to\\java.exe"} value={settingServer.java_path} onChange={(event) => {
+                        setSettingServer((oldSettingServer) => ({
+                            ...oldSettingServer,
+                            java_path: event.target.value
+                        }))
+                    }}/>
+                </SettingContainer>
+                <SettingContainer name={"Launch Arguments"} description={"Java arguments used to launch the server."}>
+                    <Input type={"text"} value={settingServer.launch_args} onChange={(event) => {
+                        setSettingServer((oldSettingServer) => ({
+                            ...oldSettingServer,
+                            launch_args: event.target.value
+                        }))
+                    }}/>
+                </SettingContainer>
+                <SettingContainer name={"Allocated RAM"} description={"Maximum RAM that can be used on the server"}>
+                    <Input type={"text"} value={settingServer.allocated_ram} onChange={(event) => {
+                        setSettingServer((oldSettingServer) => ({
+                            ...oldSettingServer,
+                            allocated_ram: event.target.value
+                        }))
+                    }}/>
+                </SettingContainer>
             </div>
             <div className={"flex flex-row w-full space-x-4"}>
                 <Button onClick={() => applySettings()} color={"orange"}>
