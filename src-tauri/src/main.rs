6ec92d1b8f8e12e7b4ee72f18bd976742a358c17
shiiru <shiiru@shiiru.dev>
2026-08-01 17:07:00 +0200

    feat(linux): disable DMABUF renderer on NVIDIA GPUs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "linux")]
+fn has_nvidia_gpu() -> bool {
+    std::fs::metadata("/sys/module/nvidia").is_ok()
+}
+
 fn main() {
+    #[cfg(target_os = "linux")]
+    if has_nvidia_gpu() {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+
     mineager_lib::run()
 }
